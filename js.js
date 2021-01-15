3c483c314f9d0ba9dce3d50dcc9a9dd2862aae5c
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2021-01-15 20:31:42 +0100

    chessboard array in JS

--- js.js
+++ js.js
@@ -3,3 +3,17 @@ function myFunction(){
 	element.classList.toggle('chess-field-marked');
 }
 
+var chessboard;
+
+chessboard =	[[	'bR',	'bN',	'bB',	'bQ',	'bK',	'bB',	'bN',	'bR'],
+				 [	'bP',	'bP',	'bP',	'bP',	'bP',	'bP',	'bP',	'bP'],
+				 [	null,	null,	null,	null,	null,	null,	null,	null],
+				 [	null,	null,	null,	null,	null,	null,	null,	null],
+				 [	null,	null,	null,	null,	null,	null,	null,	null],
+				 [	null,	null,	null,	null,	null,	null,	null,	null],
+				 [	'wP',	'wP',	'wP',	'wP',	'wP',	'wP',	'wP',	'wP'],
+				 [	'wR',	'wN',	'wB',	'wQ',	'wK',	'wB',	'wN',	'wR']];
+
+
+console.log(chessboard);
+
