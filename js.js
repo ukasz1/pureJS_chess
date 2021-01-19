5c383fc6d2d5906b8a8390f833bba548abeb7a58
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2021-01-19 19:49:41 +0100

    deleting the chess pieces from array

--- js.js
+++ js.js
@@ -17,8 +17,13 @@ chessboard =	[[	'bR',	'bN',	'bB',	'bQ',	'bK',	'bB',	'bN',	'bR'],
 
 console.log(chessboard);
 
+var firstClick;
+var secondClick;
+var clickNo=-1;
 
-function zaznaczenie(e){	//funkcja zaznaczająca pole szachowe
+function marking(e){	//funkcja zaznaczająca pole szachowe
+	
+	clickNo++;
 	var el;
 	
 	el=e.target;	//pobranie węzła obiektu zdarzenia
@@ -33,18 +38,60 @@ function zaznaczenie(e){	//funkcja zaznaczająca pole szachowe
 	var chessFieldColor;
 	chessFieldColor=el.className[17];
 	
-	if(chessFieldColor==='2'){							//klasa color1 czy color2?
+	if(chessFieldColor==='2'){							// color1 czy color2?
 		el.classList.toggle('chess-field-marked-green');
 	}
 	else{
 		el.classList.toggle('chess-field-marked-white');
 	}
+	
+	if(clickNo%2==0){		//kliknięcie pierwszego pola
+		firstClick=el.id;
+		
+		console.log(firstClick);
+		console.log(clickNo);
+		
+		var iIndex=firstClick.charCodeAt(0);	//dekodowanie adresu szachowego celem pobrania indexów
+		var jIndex=firstClick.charCodeAt(1);
+		
+		let i=0;
+		
+		do{
+			if(56-i!=jIndex)	//56 - kod ASCII dla znaku '0'
+				i++;
+			else
+				break;
+		} while(true);
+		
+		let j=0;
+		
+		do{
+			if(j+97!=iIndex)	//97 - kod ASCII dla znaku 'a'
+				j++;
+			else
+				break;
+		} while(true);
+		
+		chessboard[i][j]=null;	//sprawdzenie
+				
+	}
+	else{
+		/*
+		
+		//KLIKNIĘCIE DRUGIEGO POLA
+		
+		*/
+	}
+	console.log(chessboard);
+	
 }
 
 var pole=document.getElementsByClassName('chess-field');
 
 for(let i=0; i<64;i++){
-	pole[i].addEventListener('click', zaznaczenie, true);}
+	pole[i].addEventListener('click', marking, true);}
+
+
 
 
 
