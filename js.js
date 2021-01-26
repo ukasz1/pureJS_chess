465324a6e5cb37fcd84862fe14769bee8730e243
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2021-01-26 18:25:52 +0100

    bishop movement function completed

--- js.js
+++ js.js
@@ -332,9 +332,8 @@ function rook(i1,j1,chessboard){
 	}
 	
 
-	console.log(tab);
-return tab;
-
+	//console.log(tab);
+	return tab;
 } 
 //-----------------------------------------------------------------------------------------------------
 function bishop(i1,j1,chessboard){
@@ -342,21 +341,18 @@ function bishop(i1,j1,chessboard){
 	
 	//Sprawdzenie istnienia pól szachowych
 	var mtr=1;
-	console.log('Funkcja goniec');
 	
+	console.log('Prawo-dół:');
 	while(i1+mtr<8){
-		if(chessboard[i1+mtr][j1+mtr]==null){
+		
+		if(chessboard[i1+mtr][j1+mtr]==null && j1+mtr<8){
 			tab.push([i1+mtr,j1+mtr]);
 		}
-		else{
-			console.log('Koniec funkcji');
-			
-			if(tab.length!=0){
-			//	console.log('tab =');
-			//	console.log(tab);
+		else{	
+			if(tab.length!=0){}
+			else{
+				console.log('Length=0:'); // do usunięcia w obu funkcjach
 			}
-			else
-				console.log('Brak ruchów prawo-dół');
 			
 			break;
 		}
@@ -366,67 +362,68 @@ function bishop(i1,j1,chessboard){
 	mtr=1;
 	
 	while(i1+mtr<8){
-		if(chessboard[i1+mtr][j1-mtr]==null){
+		
+		if(chessboard[i1+mtr][j1-mtr]==null && j1-mtr>=0){
 			tab.push([i1+mtr,j1-mtr]);
 		}
 		else{
 
 			
-			if(tab.length!=0){
-
+			if(tab.length!=0){}
+			else{
+				console.log('Length=0:');
 			}
-			else
-				console.log('Brak ruchów lewo-dół');
 			
 			break;
 		}
 		mtr++;
 	}
 	
+	
 	mtr=1;
 	
 	while(i1-mtr>=0){
-		if(chessboard[i1-mtr][j1+mtr]==null){
+		
+		if(chessboard[i1-mtr][j1+mtr]==null && j1+mtr<8){
 			tab.push([i1-mtr,j1+mtr]);
+
 		}
 		else{
 
 			
-			if(tab.length!=0){
-
+			if(tab.length!=0){}
+			else{
+				console.log('Length=0:');
 			}
-			else
-				console.log('Brak ruchów góra-prawo');
 			
 			break;
 		}
 		mtr++;
 	}
 	
+	
 	mtr=1;
 	
+	console.log('Lewo-góra:');
 	while(i1-mtr>=0){
-		if(chessboard[i1-mtr][j1-mtr]==null){
+		if(chessboard[i1-mtr][j1-mtr]==null && j1-mtr>=0){
 			tab.push([i1-mtr,j1-mtr]);
 		}
-		else{
-
-			
-			if(tab.length!=0){
-
+		else{		
+			if(tab.length!=0){}
+			else{
+				console.log('Length=0:');
 			}
-			else
-				console.log('Brak ruchów góra-lewo');
-			
+				
 			break;
 		}
 		mtr++;
 	}
 	
 
-	console.log(tab);
-return tab;
-
+	//console.log(tab);
+	
+	return tab;
 }
 
 
